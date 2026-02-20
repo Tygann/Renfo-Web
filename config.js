9084f8cd6381a00cbfa7127104aecc95130b3d7c
Tyler Keegan <Tyler@Keegan.pro>
2026-02-20 09:47:03 -0600

    Refactor configuration settings and enhance index.html for iOS smart banner support

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 window.RENFO_CONFIG = {
   MAPKIT_TOKEN: "eyJraWQiOiJGOVlQS1hBNFNEIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJYS1E0MjRIUTMzIiwiaWF0IjoxNzcxNTE1MTY3LCJvcmlnaW4iOiIqLnJlbmZvLmFwcCJ9.z-ycIQdcxWkgTZLNpAlg9N0anT14BKlDy8Wydrv-dLJVBUji7ZS4b5N08NMmkElAFDkQC36QeepOHOFdHE_t7Q",
-  ASSETS_BASE_URL: "https://assets.renfo.app",
-  IOS_SMART_BANNER_APP_ID: "6502414968",
-  IOS_SMART_BANNER_APP_ARGUMENT: "https://renfo.app"
+  ASSETS_BASE_URL: "https://assets.renfo.app"
 };
