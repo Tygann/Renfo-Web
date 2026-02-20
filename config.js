978375fedd57c905c8d85be856d105dbc0f9ba3b
Tyler Keegan <Tyler@Keegan.pro>
2026-02-20 09:17:04 -0600

    Add iOS app banner functionality and enhance index.html structure

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 window.RENFO_CONFIG = {
   MAPKIT_TOKEN: "eyJraWQiOiJGOVlQS1hBNFNEIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJYS1E0MjRIUTMzIiwiaWF0IjoxNzcxNTE1MTY3LCJvcmlnaW4iOiIqLnJlbmZvLmFwcCJ9.z-ycIQdcxWkgTZLNpAlg9N0anT14BKlDy8Wydrv-dLJVBUji7ZS4b5N08NMmkElAFDkQC36QeepOHOFdHE_t7Q",
-  ASSETS_BASE_URL: "https://assets.renfo.app"
+  ASSETS_BASE_URL: "https://assets.renfo.app",
+  IOS_APP_BANNER_ENABLED: true,
+  IOS_APP_OPEN_URL: "https://renfo.app",
+  IOS_APP_STORE_URL: "",
+  IOS_APP_NAME: "Renfo",
+  IOS_APP_SUBTITLE: "Open in the Renfo app",
+  IOS_APP_ICON_URL: "/apple-touch-icon.png"
 };
